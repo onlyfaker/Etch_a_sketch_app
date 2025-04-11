c3db8b394d8c3af08c28d226624fd1f2af1dffe9
Gemaljevic Aleksandar <67740316+onlyfaker@users.noreply.github.com>
2025-04-10 21:36:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ from turtle import Turtle, Screen
 turtle = Turtle()
 screen = Screen()
 
-
 def forward():
     turtle.forward(30)
 def backward():
     turtle.forward(-30)
 def right():
-    turtle.left(20)
-def left():
     turtle.right(20)
-screen.onkey(fun=forward, key="Up")
-screen.onkey(fun=backward, key="Down")
-screen.onkey(fun=right, key="Right")
-screen.onkey(fun=left, key="Left")
+def left():
+    turtle.left(20)
+def clear_the_screen():
+    turtle.clear()
+    turtle.penup()
+    turtle.ht()
+    turtle.speed(0)
+    turtle.goto(0,0)
+    turtle.pendown()
+    turtle.st()
+    turtle.speed('slow')
+
+screen.onkey( key="w",fun=forward)
+screen.onkey(fun=backward, key="s")
+screen.onkey(fun=right, key="d")
+screen.onkey(fun=left, key="a")
+screen.onkey(fun=clear_the_screen, key="c")
 
 
 
 screen.listen()
-
-
 screen.exitonclick()
